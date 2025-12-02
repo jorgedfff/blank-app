558f559aa703dbbfe703c9e1fd2ce16a9e4b5063
jorgedfff <a224200017@unison.mx>
2025-12-01 17:24:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,6 @@
 import streamlit as st
 
 
-st.set_page_config(page_title="Inicio - Proteínas", page_icon="🏠", layout="wide")
+st.title("Holaaaa sí funciono 😭🔥")
 
-def main():
-    st.title("🏠 Introducción a las Proteínas")
-
-    st.markdown("""
-    ## ¿Qué son las proteínas?
-    Las proteínas son macromoléculas formadas por cadenas de aminoácidos y desempeñan funciones esenciales en los seres vivos, tales como:
-    - Estructurales
-    - Catalíticas (enzimas)
-    - De transporte
-    - De señalización
-    - De defensa
-
-    *Aquí irá una imagen representativa general de una proteína (estructura 3D)*
-    """)
+st.write("Si ves esto, tu Streamlit SÍ funciona.")
