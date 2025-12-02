a1d7e5963da92e0caabba3960d265f934bf2b7ba
jorgedfff <a224200017@unison.mx>
2025-12-01 17:42:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ def main():
     """)
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Myoglobin.png/500px-Myoglobin.png")
 st.markdown(""" Representación de la estructura tridimensional digitalizada de la mioglobina""")
-    st.markdown("""
-    ---
+st.markdown("""
+    
     ## Ejemplos de tipos de proteínas
 
     ### 1. Proteínas Fibrosas
